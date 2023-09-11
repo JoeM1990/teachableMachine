4f1d399664b13ccbae0d9f3cae80c199d9e98b1c
Joe Monkila <jnkiwa25@gmail.com>
2023-09-11 14:53:18 +0100

    changed id for peer

--- js/script.js
+++ js/script.js
@@ -341,7 +341,7 @@ async function loadMobileNetFeatureModel() {
   let idUser;
   let conn;
 
-  var peer = new Peer('pc1Monkila',{
+  var peer = new Peer({
     secure:true,
     host: 'https://joem1990.github.io/teachableMachine/',
   });
